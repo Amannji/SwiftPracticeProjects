4a6ce7cda199ca9271b13a671c524aad3913771d
Decoy101 <00amangpt00@gmail.com>
2024-01-10 23:51:06 +0530

    just a followup to previous commit

--- SwiftConcurrency/SwiftConcurrency/AsyncLetBootcamp.swift
+++ SwiftConcurrency/SwiftConcurrency/AsyncLetBootcamp.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct AsyncLetView: View {
+    @State var images:[UIImage] = []
+    let columns = [GridItem(.flexible()),GridItem(.flexible())]
+    let url = URL(string:"https://picsum.photos/200")!
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(images, id: \.self){ image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height:150)
+                        
+                        
+                    }
+                }
+                
+            }
+            .navigationTitle("🧠Async Let")
+        
+        }
+        .onAppear{
+            Task{
+                do{
+                    
+                    async let fetchImage1 = fetchImage()
+                    async let fetchImage2 = fetchImage()
+                    async let fetchImage3 = fetchImage()
+                    async let fetchImage4 = fetchImage()
+                    
+                    let (image1,image2,image3,image4) = await (try fetchImage1,try fetchImage2,try fetchImage3, try fetchImage4)
+                    
+                    self.images.append(contentsOf:[image1,image2,image3,image4])
+                    
+                    
+//                    let image1 = try await fetchImage()
+//                    self.images.append(image1)
+//                    
+//                    let image2 = try await fetchImage()
+//                    self.images.append(image2)
+//                    
+//                    let image3 = try await fetchImage()
+//                    self.images.append(image3)
+//                    
+//                    let image4 = try await fetchImage()
+//                    self.images.append(image4)
+                }
+            }
+        }
+        
     }
+    
+    func fetchImage() async throws -> UIImage{
+        
+        do{
+            let (data,_) = try await URLSession.shared.data(from: url,delegate: nil)
+            if let image = UIImage(data: data){
+                return image
+            }
+            else{
+                throw URLError(.badURL)
+                
+            }
+            
+        }
+        catch{
+            throw error
+        }
+        }
+    
 }
 
 #Preview {

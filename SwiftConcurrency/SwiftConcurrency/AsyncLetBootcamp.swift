269b77bdc37890809f314329b46d382dad6037a0
Decoy101 <00amangpt00@gmail.com>
2024-01-10 23:50:31 +0530

    Here I learnt how to  perform multiple async requests concurrently in just one task

---
+++ SwiftConcurrency/SwiftConcurrency/AsyncLetBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncLetView.swift
+//  SwiftConcurrency
+//
+//  Created by Aman Gupta on 10/01/24.
+//
+
+import SwiftUI
+
+struct AsyncLetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncLetView()
+}

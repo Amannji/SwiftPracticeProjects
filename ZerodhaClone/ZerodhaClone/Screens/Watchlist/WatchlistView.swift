c640094054a0df053fbe40596ccdea9d2595280e
Decoy101 <00amangpt00@gmail.com>
2024-01-19 16:59:43 +0530

    watchlist: top tab bar done with scroll and sliding animation

--- ZerodhaClone/ZerodhaClone/Screens/Watchlist/WatchlistView.swift
+++ ZerodhaClone/ZerodhaClone/Screens/Watchlist/WatchlistView.swift
@@ -7,24 +7,72 @@
 
 import SwiftUI
 
+struct CardItem: Identifiable{
+    let id: Int
+}
+
 struct WatchlistView: View {
-    
+    let cards = [CardItem(id:0),CardItem(id:1), CardItem(id:2),CardItem(id:3)]
     @State var listChoice:String = ""
+    @State private var selectedTabIndex:Int = 1
     var body: some View {
         NavigationView{
-            ScrollView(.horizontal,showsIndicators: false){
-                HStack{
-                    ForEach(1...7, id: \.self){ i in
-                        tabListChoice(text:"Watchlist \(i)")
+            VStack{
+                ScrollView(.horizontal,showsIndicators: false){
+                    HStack(spacing:25){
+                        ForEach(1...7, id: \.self){ i in
+                            Text("Watchlist \(i)")
+                                .foregroundColor(selectedTabIndex == i ? Color.blue : Color.primary)
+                                .onTapGesture{
+                                    withAnimation{
+                                        selectedTabIndex = i
+                                    }
+                                }
+                            
+                        }
+                        
+                    }
+                    .padding()
+                    GeometryReader{ geometry in
+                        Rectangle()
+                            .foregroundColor(Color.blue)
+                            .frame(width: geometry.size.width/7,height: 3)
+                            .offset(x:CGFloat(selectedTabIndex - 1)*(geometry.size.width/7),y:-15)
+                            .animation(.easeInOut,value: selectedTabIndex)
+                        
+                    }
+                    
+                }
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    LazyHStack{
+                        ForEach(1...7, id: \.self){ i in
+                            RoundedRectangle(cornerRadius:10.0)
+                                .fill(.ultraThinMaterial)
+                                .padding(.horizontal,15)
+                                .containerRelativeFrame(.horizontal)
+                                .id(i)
+                        }
+                        
                     }
                 }
-                .padding()
+                .onAppear{
+                    scrollToCardView(id: selectedTabIndex)
+                }
+                
             }
+            
+           
         }
-        
-        
-        
     }
+    
+    private func scrollToCardView(id: Int){
+        withAnimation{
+            listChoice = "Watchlist \(id)"
+        }
+    }
+    
+   
 }
 
 #Preview {
@@ -32,15 +80,5 @@ struct WatchlistView: View {
 }
 
 
-struct tabListChoice: View{
-    var text: String
-    var body: some View{
-        Button{
-            
-        }
-    label:{
-        Text(text)
-            .foregroundColor(Color.primary)
-    }
-    }
-}
+
+
